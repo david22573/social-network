ddc66d906b32117286297382baf9f192d2962733
david22573 <davidmiguel22573@gmail.com>
2023-05-30 00:58:22 -0700

    Start project and create signup/login views

---
+++ sn_frontend/src/views/LoginView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
+        <div class="main-left">
+            <div class="p-12 bg-white border border-gray-200 rounded-lg">
+                <h1 class="mb-6 text-xl">Log in</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis hic consequuntur, mollitia natus repudiandae quos deleniti laudantium aliquam officiis sint suscipit blanditiis expedita repellat eligendi ipsa nostrum architecto quas quae.</p>
+                <p class="font-bold">
+                    Don't have an account? <a href="#" class="underline">Click here</a> to log create one!
+                </p>
+            </div>
+        </div>
+        <div class="main-right">
+            <div class="p-12 bg-white border border-gray-200 rounded-lg">
+                <form class="space-y-6">
+                    <div>
+                        <label for="email">E-mail</label>
+                        <input
+                            type="email"
+                            placeholder="Your e-mail address"
+                            class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"    
+                        />
+                    </div>
+                    <div>
+                        <label for="email">Password</label>
+                        <input
+                            type="password"
+                            placeholder="Your password"
+                            class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"    
+                        />
+                    </div>
+                    <div>
+                        <button class="py-4 px-6 bg-purple-600 text-white rounded-lg">Log In</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
